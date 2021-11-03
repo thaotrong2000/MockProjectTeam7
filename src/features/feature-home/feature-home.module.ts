f8335f36e2f28c1de1a19599d4612948d2ff725c
thaotrong2000 <trongthaouet@gmail.com>
2021-11-03 13:23:40 +0700

    merge from master

---
+++ src/features/feature-home/feature-home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureHomeRoutingModule } from './feature-home-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureHomeRoutingModule
+  ]
+})
+export class FeatureHomeModule { }
---
+++ src/features/feature-login/feature-login.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureLoginRoutingModule } from './feature-login-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureLoginRoutingModule
+  ]
+})
+export class FeatureLoginModule { }
