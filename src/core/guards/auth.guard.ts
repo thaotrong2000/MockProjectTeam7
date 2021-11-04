00122710377a287af344df12c3fbeb28edc2a63c
thaotrong2000 <trongthaouet@gmail.com>
2021-11-04 19:26:54 +0700

    auth user from /login. Then get Token - ThaoNT119

--- src/core/guards/auth.guard.ts
+++ src/core/guards/auth.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor() {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (localStorage.getItem('token')) {
+      console.log('da dang nhap');
+      return false;
+    }
     return true;
   }
-  
 }
--- src/modules/AuthModule/login/login.component.ts
+++ src/modules/AuthModule/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/services/AuthService/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public login(): void {
+    this.authService.getToken().subscribe((data) => {
+      localStorage.setItem('token', data.user.token);
+      console.log('Login Success');
+      this.router.navigate(['']);
+    });
   }
-
 }
