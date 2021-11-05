79631e50ac5a19941e32f3fb03f169beac505492
thaotrong2000 <trongthaouet@gmail.com>
2021-11-06 02:24:09 +0700

    fix check login and get data from api for HomePage

--- src/modules/HomeModule/home/home.component.ts
+++ src/modules/HomeModule/home/home.component.ts
@@ -9,12 +9,11 @@ import { StoreService } from 'src/core/services/store.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   articlesArray: any = [];
 
   limit: number = 10;
 
-  offset : number = 0;
+  offset: number = 0;
 
   array: any = [];
 
@@ -22,20 +21,23 @@ export class HomeComponent implements OnInit {
 
   paginationArticle: any = [];
 
-  constructor(private readonly articleService: ArticleService) {}
+  demoArray: Array<any> = [0, 1, 2, 3, 4];
+  checkLogin: boolean = false;
+
+  constructor(
+    private readonly articleService: ArticleService,
+    private storeService: StoreService
+  ) {}
 
   ngOnInit(): void {
     // this.getListArticlesGlobal();
-    this.articleService.getArticleLimitAndOffset(this.limit, this.offset).subscribe(articles => {
-      this.paginationArticle = articles;
-      console.log('pagi', this.paginationArticle);
-    })
+    this.articleService
+      .getArticleLimitAndOffset(this.limit, this.offset)
+      .subscribe((articles) => {
+        this.paginationArticle = articles;
+        console.log('pagi', this.paginationArticle);
+      });
 
-  demoArray: Array<any> = [0, 1, 2, 3, 4];
-  checkLogin: boolean = false;
-  constructor(private storeService: StoreService) {}
-
-  ngOnInit(): void {
     this.checkStatusLogin();
     if (this.checkLogin) {
       console.log('Bạn đã đăng nhập');
@@ -59,18 +61,20 @@ export class HomeComponent implements OnInit {
       this.offset += 10;
       console.log('Bạn đã load thêm vài dữ liệu');
 
-      this.articleService.getArticleLimitAndOffset(this.limit, this.offset).subscribe(articles => {
-        this.paginationArticle = articles;
-        console.log('pagination', this.paginationArticle);
-      })
+      this.articleService
+        .getArticleLimitAndOffset(this.limit, this.offset)
+        .subscribe((articles) => {
+          this.paginationArticle = articles;
+          console.log('pagination', this.paginationArticle);
+        });
       this.demoArr.push(10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
     }
   }
 
-  getListArticlesGlobal(){
-    this.articleService.getListArticles().subscribe(articles => {
+  public getListArticlesGlobal() {
+    this.articleService.getListArticles().subscribe((articles) => {
       console.log('list:', articles.articles);
       this.articlesArray = articles.articles;
-    })
+    });
   }
 }
