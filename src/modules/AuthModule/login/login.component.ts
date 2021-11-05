6927317faeb79fdb3d0438830117a11ac02ea1af
QuangMN <quangxum69@gmail.com>
2021-11-05 10:33:37 +0700

    login_ui

--- src/modules/AuthModule/login/login.component.ts
+++ src/modules/AuthModule/login/login.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/services/AuthService/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  constructor() { }
 
-  public login(): void {
-    this.authService.getToken().subscribe((data) => {
-      localStorage.setItem('token', data.user.token);
-      console.log('Login Success');
-      this.router.navigate(['']);
-    });
+  ngOnInit(): void {
   }
+
+  public email!: string;
+  public password!: any;
+
+  public checkSubmit(ngForm: NgForm): void {
+    if (ngForm.invalid) {
+      alert('Form is invalid');
+    } else {
+      alert('Form is submitted');
+    }
+  }
+
 }
