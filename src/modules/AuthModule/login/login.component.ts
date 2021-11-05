0cd363f3731e9ef6cb60508ac5c743341af474af
thaotrong2000 <trongthaouet@gmail.com>
2021-11-06 02:02:48 +0700

    Check status Login in All web

--- src/modules/AuthModule/login/login.component.ts
+++ src/modules/AuthModule/login/login.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { StoreService } from 'src/core/services/store.service';
+import { AuthService } from 'src/services/AuthService/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private storeService: StoreService,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public email!: string;
   public password!: any;
 
   public checkSubmit(ngForm: NgForm): void {
-    if (ngForm.invalid) {
-      alert('Form is invalid');
-    } else {
-      alert('Form is submitted');
-    }
+    this.authService.getToken().subscribe((data) => {
+      localStorage.setItem('token', data.user.token);
+      if (localStorage.getItem('token')) {
+        this.router.navigate(['']);
+      }
+      this.storeService.setToken(data.user.token);
+    });
   }
-
 }
---
+++ src/modules/EditorModule/editor-article/editor-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-article',
+  templateUrl: './editor-article.component.html',
+  styleUrls: ['./editor-article.component.css']
+})
+export class EditorArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
