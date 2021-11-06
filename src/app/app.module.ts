91c844f020aa63f62a28adc927e65b6bb3b6f0eb
QuangMN <quangxum69@gmail.com>
2021-11-06 19:36:31 +0700

    thisone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { HomeModule } from 'src/modules/HomeModule/home.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProfileModule } from 'src/modules/ProfileModule/profile.module';
 import { EditorModule } from 'src/modules/EditorModule/editor.module';
+import { NavbarComponent } from 'src/core/components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/modules/HomeModule/home.module.ts
+++ src/modules/HomeModule/home.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from 'src/core/components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [HomeComponent,NavbarComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
