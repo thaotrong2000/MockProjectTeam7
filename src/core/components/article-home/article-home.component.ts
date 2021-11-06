fecf50fdce1e06e216b85bca1519ae4813e38807
thaotrong2000 <trongthaouet@gmail.com>
2021-11-07 04:31:05 +0700

    Template Homepage

---
+++ src/core/components/article-home/article-home.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-home',
+  templateUrl: './article-home.component.html',
+  styleUrls: ['./article-home.component.css'],
+})
+export class ArticleHomeComponent implements OnInit {
+  @Input() nameAuthor: string = '';
+  @Input() srcImage: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() body: string = '';
+  @Input() tag: any = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
