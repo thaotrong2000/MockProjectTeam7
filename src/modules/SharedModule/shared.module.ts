592f29fddd5d46505ae01a5a5428e535cd3324c0
thaotrong2000 <trongthaouet@gmail.com>
2021-11-08 14:00:16 +0700

    custom Tag - with Reactive Form

--- src/modules/SharedModule/shared.module.ts
+++ src/modules/SharedModule/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from 'src/core/components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule, RouterModule, NgbModule],
-  exports: [NavbarComponent, NgbModule],
+  imports: [CommonModule, RouterModule, NgbModule, ReactiveFormsModule],
+  exports: [NavbarComponent, NgbModule, ReactiveFormsModule],
 })
 export class SharedModule {}
