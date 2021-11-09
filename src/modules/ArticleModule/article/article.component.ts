052ba2ffcb12cc89b36aafe640bac54ffa529af6
QuangMN <quangxum69@gmail.com>
2021-11-09 12:57:50 +0700

    commit on master

--- src/modules/ArticleModule/article/article.component.ts
+++ src/modules/ArticleModule/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+
+  @Input() nameAuthor: string = '';
+  @Input() srcImage: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() body: string = '';
+  @Input() tag: any = [];
+  @Input() checkLike: boolean = false;
 
   constructor() { }
 
