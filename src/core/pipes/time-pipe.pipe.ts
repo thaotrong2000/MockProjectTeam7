073aa2e957528b5ada8883257cae1585cca772ae
thaotrong2000 <trongthaouet@gmail.com>
2021-11-15 00:43:16 +0700

    pipe handle time

--- src/core/pipes/time-pipe.pipe.ts
+++ src/core/pipes/time-pipe.pipe.ts
@@ -6,17 +6,34 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimePipePipe implements PipeTransform {
   transform(value: any, ...args: unknown[]): unknown {
     var date: Date = new Date(value.toString().slice(0, 10));
+    console.log(date);
     var dateTotal: number = date.getTime();
-    console.log(dateTotal);
     var arrTime = [];
     arrTime = value
       .toString()
       .slice(11, value.length - 1)
       .split(':');
     var totalHour: number =
-      1000 * (arrTime[0] * 60 * 60 + arrTime[1] * 60 + arrTime[2]);
-    console.log(dateTotal + totalHour);
+      1000 *
+      (Number(arrTime[0]) * 60 * 60 +
+        Number(arrTime[1]) * 60 +
+        Number(arrTime[2]));
+    var timePost: number = Date.now() - (dateTotal + totalHour);
+    console.log(timePost);
+    if (timePost < 120000) {
+      return '1 minute';
+    }
 
-    return value;
+    if (timePost < 3600000) {
+      return Math.round(timePost / 60000) + ' minutes';
+    }
+    if (timePost < 7200000) {
+      return '1 hour';
+    }
+    if (timePost < 86400000) {
+      return Math.round(timePost / 3600000) + ' hours';
+    } else {
+      return Math.round(timePost / 86400000) + ' days';
+    }
   }
 }
