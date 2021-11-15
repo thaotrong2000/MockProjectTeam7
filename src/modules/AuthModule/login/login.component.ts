10d5cea82d5bf0f21203652673c9d69f312dd56e
thaotrong2000 <trongthaouet@gmail.com>
2021-11-15 17:06:37 +0700

    edit template login & register

--- src/modules/AuthModule/login/login.component.ts
+++ src/modules/AuthModule/login/login.component.ts
@@ -10,7 +10,7 @@ import { LoginService } from 'src/services/LoginService/login.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit, AfterViewInit {
+export class LoginComponent implements OnInit {
   formGroup = this.fb.group({
     userName: this.fb.control('', Validators.required),
     passWord: this.fb.control('', Validators.required),
@@ -28,11 +28,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.loginService.setCheckRouterLogin(true);
-  }
-
-  ngAfterViewInit(): void {
-    (document.querySelector('.mat-typography') as HTMLElement).style.overflowY =
-      'hidden';
+    this.storeService.setUrlCurrent(this.router.url);
   }
 
   public checkSubmit(): void {
