52853d8ed1b497109755a987b01822e782188365
gianggiang <giangnguyenllhl@gmail.com>
2021-11-16 14:10:59 +0700

    okkk

--- src/modules/ProfileModule/profile.module.ts
+++ src/modules/ProfileModule/profile.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile/profile.component';
-import { ProfileFavoritesComponent } from './profile-favorites/profile-favorites.component';
 import { SharedModule } from '../SharedModule/shared.module';
 import { ArticleHomeComponent } from 'src/core/components/article-home/article-home.component';
 import { HomeModule } from '../HomeModule/home.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ProfileComponent, ProfileFavoritesComponent],
+  declarations: [ProfileComponent],
   imports: [CommonModule, ProfileRoutingModule, SharedModule, HomeModule, ReactiveFormsModule ],
   exports: [SharedModule]
 })
